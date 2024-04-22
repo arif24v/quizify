01303fd12f284561fc2ea53727118cd99abfc05a
rwiriya <rwiriya123@gmail.com>
2024-04-22 15:19:19 -0400

    login

--- quizify/src/app/login/page.js
+++ quizify/src/app/login/page.js
@@ -1,26 +1,53 @@
 export default function Page() {
-    return (
-        <main>
-            <div className="absolute left-100 top-100">
-                
-                <button onclick={signup("LI")}>Log In</button>
-                <button onclick={signup("SU")}>Sign Up</button>
-            </div>
-            <input type="text" className="rounded-sm border-2 w-full h-7 font-mono text-sm outline-none p-2" placeholder="Username"/>
-            <div>
-                <img src="imgplaceholder.jpg" width="610" height="720"></img>
-            </div>
-        </main>
-    );
-    const s = "LI";
-    function signup(sign){
-        if(!s.equals(sign)){
-            s = sign;
-            if(s.equals("LI")){
-                
-            } else if(s.equals("SU")){
-
+    var state = "L";
+    function switchMethod(m){
+        if(!m===state){
+            if(m==="L"){
+                console.log("logging in");
+                state = "L";
+                document.getElementById("submit").innerHTML="Register";
+            } else{
+                console.log("signing up");
+                state = "S";
+                document.getElementById("submit").innerHTML="Sign Up";
             }
         }
     }
+    return (
+        <main>
+            <head>
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+                </link>
+            </head>
+            <body>
+                <div class="row">
+                <div id="image" class=".col-6 .col-sm-3">
+                    <img src="imgplaceholder.jpg" width="100px" height="100px"></img>
+                </div>
+                <div id="loginscreen" class=".col-6 .col-sm-3">
+                    <button type="button" onclick={switchMethod("L")}>Log In</button>
+                    <button type="button" onclick={switchMethod("S")}>Sign Up</button>
+                <form>
+                    <div>
+                        <h1 class="display-3">Log In</h1> 
+                    </div>
+                    <div class="form-row align-items-center">
+                        <label for="inputUser" class="h4">Username</label>
+                        <input type="text" class="form-control" id="inputUser" placeholder="Username"></input>
+                    </div>
+                    <div class="form-group">
+                        <label for="inputPass" class="h4">Password</label>
+                        <input type="text" class="form-control" id="inputPass" placeholder="Password"></input>
+                    </div>
+ 
+                    <button type="submit" class="btn btn-primary" id="submit">Sign in</button>
+                </form>
+                </div>
+                </div>
+            </body>
+        </main>
+    );
 }
+
+
+
