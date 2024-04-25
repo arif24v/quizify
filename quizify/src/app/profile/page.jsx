af014e84fe40fd210f0d9fe0a874629950509b4b
James Li <106193725+jame0725@users.noreply.github.com>
2024-04-25 13:58:52 -0400

    man

--- quizify/src/app/profile/page.jsx
+++ quizify/src/app/profile/page.jsx
@@ -1,8 +1,25 @@
+//import profile from "./../../../../public/profile.jpg"
+
 export default function Page() {
     return (
         <main>
-            Profile
-            <img class="h-10 w-10 rounded-full" src="quizify/.next/public/profile.jpg" alt="profile.jpg" />
+            <p class="mt-8 mb-10 text-7xl text-center"> Profile </p>
+            <div class="gap-4 columns-2">
+                <div>
+                    <img class="ml-20 h-30 w-30 rounded-full" src= "https://static.vecteezy.com/system/resources/previews/018/765/757/original/user-profile-icon-in-flat-style-member-avatar-illustration-on-isolated-background-human-permission-sign-business-concept-vector.jpg" alt="profile.jpg" />
+                </div>
+                <div>
+                    <p class="text-5xl">
+                        Username:
+                    </p> 
+                </div>
+                <div>
+
+                </div>
+                <div>
+                    <input type="text" class="form-control" id="inputPass" placeholder=""></input>
+                </div>
+            </div>
         </main>
     );
 }
