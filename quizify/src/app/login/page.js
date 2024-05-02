dee9eddeadb99c951ce5bde62bfc369a273bd797
James Li <106193725+jame0725@users.noreply.github.com>
2024-05-02 07:51:56 -0400

    log in page changes

--- quizify/src/app/login/page.js
+++ quizify/src/app/login/page.js
@@ -7,42 +7,63 @@ import Link from "next/link"
 export default function Page() {
     let state = "L";
     const [stateIn, setState] = useState("Log In");
-    async function switchMethod(m){
+    async function switchMethod(m) {
         console.log("test");
-        if(!m===state){
-            if(m==="L"){
+        if (!m === state) {
+            if (m === "L") {
                 console.log("logging in");
                 state = "L";
-                title.innerText="Log In";
-            } else{
+                title.innerText = "Log In";
+            } else {
                 console.log("signing up");
                 state = "S";
-                title.innerText="Sign Up";
+                title.innerText = "Sign Up";
             }
         }
     }
     return (
         <main>
-            <div>
-                
-                /*kinda need to get rid of this line cuz its making all the buttons highlight blue and ignore our original css but getting rid of it makes the page go weird*/
-                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous" />
-            </div>   
             {<div>
-                <div className="flex flex-row w-full l-[50vw]">
-                    <div className="w-[45vw] h-[100vh] flex flex-row">
-                        <img src="books.jpg" className="w-full h-full" />
+                {
+                    
+                <div class="gap-4 columns-2">
+                    <div>
+                        <img class="h-[93vh] w-[50vw]" src="books.jpg" alt="books.jpg" />
+                    </div>
+                    <div className="h-[30vh] w-full items-center justify-center flex flex-col align-items">
+                        <div class="pt-20 text-5xl font-mono"> LOG IN </div>
                     </div>
                     <div>
-                        <div class="h-[4vw]">
-                            
-                            <button class="btn position-absolute start-[60vw] hover:underline text-brown font-mono" type="button" onClick={() => setState("Log In")}>Log In</button>
-                            <button class="btn position-absolute start-[80vw] hover:underline text-brown font-mono" type="button" onClick={() => setState("Sign Up")}>Sign Up</button>
+                    </div>
+
+                    <div>
+                        <div class="ml-40 text-3xl text-brown font-mono">
+                            Username:
                         </div>
-                        
-                        <form className="flex flex-col position-absolute start-[60vw]">
+                    </div>
+                </div>
+                }
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                {/* <div className="flex flex-row">
+                    <div className="flex flex-row">
+                        <img src="books.jpg" className="w-100 h-100" />
+                    </div>
+                    <div>
+
+                            <button className="ml-25 hover:underline text-brown font-mono" type="button" onClick={() => setState("Log In")}>Log In</button>
+                            <button className="ml-25 hover:underline text-brown font-mono" type="button" onClick={() => setState("Sign Up")}>Sign Up</button>
+
+                        <form className="flex flex-col">
                             <div class="mx-auto">
-                                <h1 class="display-3 h-[10vw]" id="title">{stateIn}</h1> 
+                                <h1 class="display-3 h-[10vw]" id="title">{stateIn}</h1>
                             </div>
                             <div class="form-row align-items-center">
                                 <label for="inputUser" class="h4">Username</label>
@@ -56,11 +77,8 @@ export default function Page() {
                             <button type="submit" class="btn btn-primary" id="submit">{stateIn}</button>
                         </form>
                     </div>
-                </div>
+                </div> */}
             </div>}
         </main>
     );
 }
-
-
-
