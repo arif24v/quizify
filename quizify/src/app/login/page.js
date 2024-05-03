2c2d66ea3a164074d7cd1319492b7c37aaad5b37
rwiriya <rwiriya123@gmail.com>
2024-05-03 13:48:54 -0400

    Update page.js

--- quizify/src/app/login/page.js
+++ quizify/src/app/login/page.js
@@ -1,9 +1,9 @@
-"use client"
+"use server"
 
 import { useState } from "react";
 import ReactDOM from "react-dom/client";
 import Link from "next/link"
-import { getFirestore, collection, addDoc } from 'firebase/firestore';
+import { collection, addDoc } from 'firebase/firestore';
 import { doc, getDoc } from "firebase/firestore";
 import { db } from "./../firebaseLogin.js"
 export default function Page() {
@@ -42,64 +42,39 @@ async function signin(u,p){
     }
 }
     const [stateIn, setState] = useState("Log In");
-    async function switchMethod(m) {
-        console.log("test");
-        if (!m === state) {
-            if (m === "L") {
-                console.log("logging in");
-                state = "L";
-                title.innerText = "Log In";
-            } else {
-                console.log("signing up");
-                state = "S";
-                title.innerText = "Sign Up";
+    async function onclick(u,p){
+        if(inputUser.value!==""&&inputPass.value!==""){
+            if(stateIn==="Log In"){ //log in
+                signin(u,p);
+            } else{ //signup
+                register(u,p);
             }
         }
     }
     
     return (
         <main>
+            <div>
+                
+                /*kinda need to get rid of this line cuz its making all the buttons highlight blue and ignore our original css but getting rid of it makes the page go weird*/
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous" />
+            </div>   
             {<div>
-                {
-                    
-                <div class="gap-4 columns-2">
-                    <div>
-                        <img class="h-[93vh] w-[50vw]" src="books.jpg" alt="books.jpg" />
-                    </div>
-                    <div className="h-[30vh] w-full items-center justify-center flex flex-col align-items">
-                        <div class="pt-20 text-5xl font-mono"> LOG IN </div>
+                <div className="flex flex-row w-full l-[50vw]">
+                    <div className="w-[45vw] h-[100vh] flex flex-row">
+                        <img src="books.jpg" className="w-full h-full" />
                     </div>
                     <div>
-                    </div>
-
-                    <div>
-                        <div class="ml-40 text-3xl text-brown font-mono">
-                            Username:
+                        <div class="h-[4vw]">
+                            
+                            <button class="btn position-absolute start-[60vw] hover:underline text-brown font-mono" type="button" onClick={() => setState("Log In")}>Log In</button>
+                            <button class="btn position-absolute start-[80vw] hover:underline text-brown font-mono" type="button" onClick={() => setState("Sign Up")}>Sign Up</button>
                         </div>
-                    </div>
-                </div>
-                }
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                {/* <div className="flex flex-row">
-                    <div className="flex flex-row">
-                        <img src="books.jpg" className="w-100 h-100" />
-                    </div>
-                    <div>
-
-                            <button className="ml-25 hover:underline text-brown font-mono" type="button" onClick={() => setState("Log In")}>Log In</button>
-                            <button className="ml-25 hover:underline text-brown font-mono" type="button" onClick={() => setState("Sign Up")}>Sign Up</button>
-
-                        <form className="flex flex-col">
+                        
+                        <form className="flex flex-col position-absolute start-[60vw]">
+                        
                             <div class="mx-auto">
-                                <h1 class="display-3 h-[10vw]" id="title">{stateIn}</h1>
+                                <h1 class="display-3 h-[10vw]" id="title">{stateIn}</h1> 
                             </div>
                             <div class="form-row align-items-center">
                                 <label for="inputUser" class="h4">Username</label>
@@ -113,7 +88,7 @@ async function signin(u,p){
                             <a class="btn btn-info" role="button" onClick={() => onclick(inputUser.value, inputPass.value)}>{stateIn}</a>
                         </form>
                     </div>
-                </div> */}
+                </div>
             </div>}
         </main>
     );
